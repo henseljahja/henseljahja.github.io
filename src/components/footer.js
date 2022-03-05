72fd5bc7a00db9cda50f5aa6abec0e93fee5f4fe
Hensel Jahja <henseljahja@gmail.com>
2022-03-06 04:57:54 +0700

    credit

--- src/components/footer.js
+++ src/components/footer.js
@@ -106,7 +106,8 @@ const Footer = () => {
 
       <StyledCredit tabindex="-1">
         <a href="https://github.com/bchiang7/v4">
-          <div>Designed &amp; Built by Brittany Chiang</div>
+          <div>Huge thanks to Brittany Chiang for this website template</div>
+          {/* <div>Designed &amp; Built by Brittany Chiang</div> */}
 
           {githubInfo.stars && githubInfo.forks && (
             <div className="github-stats">
