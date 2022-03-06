55d38b4d5937068648a11319144707fba3a678a3
Hensel Jahja <henseljahja@gmail.com>
2022-03-07 00:01:56 +0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-9ef5e4fcf4e57fa3b726.js"
+    "url": "webpack-runtime-ca31506380c0cee90f59.js"
   },
   {
     "url": "framework-fbf2011d6c2b83ba228a.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "9327d2232aa58a288227465fd7857755"
+    "revision": "efe5a63cdf529953d5476275521f95bc"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-27c66d5dcd0cba7d37a1.js"
